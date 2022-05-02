3b6a3152235a685f03e9dd912b290e9b4e677933
jiwangyihao <jwyh@jwyihao.top>
2022-05-02 08:20:47 +0000

    Deploying to gh-pages from  @ feabd4de651c4a486b4b21e9516c70d02df23be6 🚀

---
+++ assets/Application.f6348306.js
@@ -0,0 +1,1 @@
+import{_ as e,q as c,s as r}from"./index.7434de75.js";const t={};function a(n,o){return c(),r("div")}var _=e(t,[["render",a]]);export{_ as default};
---
+++ assets/axios.fd163daf.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index.2885cb15.js";let a;a=e.create({baseURL:"https://store.deepinos.org/api/"});export{a as api};
