7b895900e6972411e04745453ce9821cdd4222a9
jiwangyihao <jwyh@jwyihao.top>
2022-05-22 10:02:29 +0800

    为应用卡片添加点击动画 修正了重排动画的滚动位置问题

--- src/pages/store/Sort.vue
+++ src/pages/store/Sort.vue
@@ -32,6 +32,7 @@ export default {
 <script setup>
 import { onUnmounted, ref } from "vue";
 import { useRoute } from "vue-router";
+import BezierEasing from "bezier-easing";
 
 const route = useRoute();
 
@@ -56,8 +57,9 @@ const containerState = ref({
   animation: false,
 });
 
-function resizeCardView(e) {
-  console.log(e);
+const column = ref(0);
+
+function resizeCardView() {
   appList.value = layoutAppCard(sourceAppList);
 }
 
@@ -72,42 +74,96 @@ function layoutAppCard(appList) {
 
   let list = JSON.parse(JSON.stringify(appList.value));
   let containerWidth = container.value.clientWidth;
-  let column = Math.floor(
+  let scrollRatio =
+    container.value.parentElement.parentElement.scrollTop /
+    container.value.clientHeight;
+  let ifScroll;
+  if (
+    column.value !==
+    Math.floor((containerWidth + gap - horizonPadding * 2) / (cardWidth + gap))
+  ) {
+    ifScroll = true;
+  }
+  column.value = Math.floor(
     (containerWidth + gap - horizonPadding * 2) / (cardWidth + gap)
   );
   let horizonGap =
-    (containerWidth - (cardWidth + gap) * column + gap - horizonPadding * 2) /
-    (column + 1);
+    (containerWidth -
+      (cardWidth + gap) * column.value +
+      gap -
+      horizonPadding * 2) /
+    (column.value + 1);
   container.value.style.height = `${
-    Math.ceil(list.length / column) * (cardHeight + gap) +
+    Math.ceil(list.length / column.value) * (cardHeight + gap) +
     gap +
     verticalPadding * 1.5
   }px`;
+  if (ifScroll) {
+    scrollAnimation(scrollRatio);
+  }
+
   list.forEach((item, index) => {
     item.position = {
       left:
-        (index % column) * (horizonGap + gap + cardWidth) +
+        (index % column.value) * (horizonGap + gap + cardWidth) +
         horizonPadding +
         horizonGap,
-      top: Math.floor(index / column) * (cardHeight + gap) + verticalPadding,
+      top:
+        Math.floor(index / column.value) * (cardHeight + gap) + verticalPadding,
     };
     item.style = `transform:translate(${item.position.left}px,${item.position.top}px)`;
+    item.class = {
+      active: false,
+      animation: false,
+    };
   });
   return list;
 }
 
-function appCardDown() {
+function scrollAnimation(scrollRatio) {
+  let start;
+  const scrollStart = container.value.parentElement.parentElement.scrollTop;
+  const scrollDistance =
+    scrollRatio * container.value.clientHeight - scrollStart;
+  const easing = BezierEasing(0.25, 0.1, 0.25, 1.0);
+  function scrollStep(timestamp) {
+    if (start === undefined) {
+      start = timestamp;
+    }
+    const elapsed = timestamp - start;
+
+    const process = elapsed / 600;
+
+    //这里使用`Math.min()`确保元素刚好停在200px的位置。
+    container.value.parentElement.parentElement.scrollTo(
+      0,
+      scrollStart + scrollDistance * easing(Math.min(process, 1))
+    );
+
+    if (process < 1) {
+      // 在0.6秒后停止动画
+      requestAnimationFrame(scrollStep);
+    }
+  }
+  requestAnimationFrame(scrollStep);
+}
+
+function appCardDown(app) {
   containerState.value.active = true;
   containerState.value.animation = true;
+  app.class.active = true;
+  app.class.animation = true;
 }
 
-function appCardUp() {
+function appCardUp(app) {
   containerState.value.active = false;
+  app.class.active = false;
 }
 
-function appCardAnimated(event) {
+function appCardAnimated(app, event) {
   if (event.propertyName === "transform") {
     containerState.value.animation = false;
+    app.class.animation = false;
   }
 }
 
@@ -117,6 +173,7 @@ onUnmounted(() => {
 </script>
 
 <template>
+  <!--suppress JSUndeclaredVariable -->
   <div
     :class="containerState"
     :ref="
@@ -128,31 +185,34 @@ onUnmounted(() => {
     <div
       v-for="(app, index) in appList"
       :style="app.style"
+      :class="app.class"
       :key="index"
       :ref="
         (el) => {
           app.el = el;
         }
       "
-      @mousedown="appCardDown"
-      @mouseup="appCardUp"
-      @transitionend="appCardAnimated"
+      @mousedown="(e) => appCardDown(app, e)"
+      @mouseup="(e) => appCardUp(app, e)"
     >
       <!-- @click="openApp(app.application_id,index)" -->
-      <img
-        :src="
-          source +
-          '/store/' +
-          sort +
-          '/' +
-          app.package.replace('+', '%2B') +
-          '/icon.png'
-        "
-        alt=""
-      />
-      <div>
-        <h6>{{ app["application_name_zh"] }}</h6>
-        <p>{{ app["more"] }}</p>
+      <div @transitionend="(e) => appCardAnimated(app, e)">
+        <!--suppress JSUnresolvedVariable -->
+        <img
+          :src="
+            source +
+            '/store/' +
+            sort +
+            '/' +
+            app.package.replace('+', '%2B') +
+            '/icon.png'
+          "
+          alt=""
+        />
+        <div>
+          <h6>{{ app["application_name_zh"] }}</h6>
+          <p>{{ app["more"] }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -161,68 +221,80 @@ onUnmounted(() => {
 <style scoped lang="scss">
 .container {
   > div {
-    display: flex;
     width: 264px;
     height: 92px;
-    background-color: white;
     position: absolute;
     top: 0;
     left: 0;
-    align-items: center;
-    justify-content: space-evenly;
-    padding: 12px;
-    border-radius: 2vmin;
-    box-shadow: 0 1px 10px #0000004d, 0 2px 4px #00000036,
-      0 3px 1px -4px #0000002e;
     transition: {
-      property: box-shadow, transform;
-      duration: 0.35s, 0.75s;
+      property: transform;
+      duration: 0.6s;
     }
-    will-change: box-shadow;
+    will-change: transform;
 
-    &:hover {
-      box-shadow: 0 1px 5px #0003, 0 2px 2px #00000024, 0 3px 1px -2px #0000001f;
-    }
+    > div {
+      display: flex;
+      background-color: white;
+      align-items: center;
+      justify-content: space-evenly;
+      padding: 12px;
+      border-radius: 2vmin;
+      box-shadow: 0 1px 10px #0000004d, 0 2px 4px #00000036,
+        0 3px 1px -4px #0000002e;
+      transition: {
+        property: box-shadow, transform;
+        duration: 0.5s, 0.25s;
+      }
+      will-change: box-shadow, transform;
 
-    img {
-      width: 64px;
-      height: 64px;
-      object-fit: contain;
-    }
-
-    div {
-      width: 170px;
-      height: 68px;
-      padding-left: 10px;
-
-      h6 {
-        font-size: 14px;
-        font-weight: 700;
-        line-height: 20px;
-        margin: 0;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        overflow: hidden;
+      &:hover {
+        box-shadow: 0 1px 5px #0003, 0 2px 2px #00000024,
+          0 3px 1px -2px #0000001f;
       }
 
-      p {
-        color: gray;
-        font-size: 13px;
-        line-height: 16px;
-        margin: 0;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 3;
-        -webkit-box-orient: vertical;
+      img {
+        width: 64px;
+        height: 64px;
+        object-fit: contain;
+      }
+
+      div {
+        width: 170px;
+        height: 68px;
+        padding-left: 10px;
+
+        h6 {
+          font-size: 14px;
+          font-weight: 700;
+          line-height: 20px;
+          margin: 0;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          overflow: hidden;
+        }
+
+        p {
+          color: gray;
+          font-size: 13px;
+          line-height: 16px;
+          margin: 0;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-line-clamp: 3;
+          -webkit-box-orient: vertical;
+        }
       }
     }
   }
 
   &.active,
   &.animation {
-    & > div {
-      transform: scale(0.9);
+    & > div.active,
+    & > div.animation {
+      > div {
+        transform: scale(0.96);
+      }
     }
   }
 }
